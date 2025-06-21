e4179f9288c6be3483a9a9b4f89741e5dd8a7924
Grace Wang <84159791+Grace273@users.noreply.github.com>
2025-06-21 16:38:52 -0400

    change display of active tabs in nav bar

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,20 @@
-function openMenuOption(menuOption) {
+function openMenuOption(evt, menuOption) { 
   var i;
   var x = document.getElementsByClassName("menu-option");
+  var tablinks
+  
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  
+  tablinks = document.getElementsByClassName("tablink");
+
+  for (i=0; i < x.length; i++) {
+    // deactivates current all tabs
+    tablinks[i].className = tablinks[i].className.replace(" menu-option-active", "")
+  }
+
+  // activate clicked tab and display its contents
   document.getElementById(menuOption).style.display = "block";
+  evt.currentTarget.className += " menu-option-active"
 }
