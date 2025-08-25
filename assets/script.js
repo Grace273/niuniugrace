05d62d1f54cc6f008e31a32e9b4ed507aaad5e52
Grace Wang <84159791+Grace273@users.noreply.github.com>
2025-08-25 17:52:19 -0400

    Add functionality to send message button (pushes message to database)

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,51 @@
-function openMenuOption(evt, menuOption) { 
-  var i;
-  var x = document.getElementsByClassName("menu-option");
-  var tablinks
-  
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  
-  tablinks = document.getElementsByClassName("tablink");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase, push, ref } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
 
-  for (i=0; i < x.length; i++) {
+// LEAVE A MESSAGE SECTION
+const firebaseConfig = {
+  databaseURL: "https://niuniugrace-917a4-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "messages")
+const sendButton = document.getElementById("send-btn")
+let confirmationEl = document.getElementById("confirmation-el")
+let inputEl = document.getElementById("input-el")
+let myMessages = []
+
+sendButton.addEventListener("click", function(){
+  if (myMessages.length === 100){
+    confirmationEl.innerHTML = "Inbox full (100 messages). Try again later!"
+  }
+  else if(inputEl.value){
+    confirmationEl.innerHTML = "Message sent!"
+    push(referenceInDB, inputEl.value)
+    inputEl.value = ""
+  }
+  else {
+    confirmationEl.innerHTML = "Write something to send!"
+  }
+})
+
+// VLOG/BLOG NAVIGATION 
+function openMenuOption(evt, menuOption) { 
+  let i
+  let x = document.getElementsByClassName("menu-option")
+  let tablinks
+
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none"
+  }
+
+  tablinks = document.getElementsByClassName("tablink")
+
+  for (let i=0; i < x.length; i++) {
     // deactivates current all tabs
     tablinks[i].className = tablinks[i].className.replace(" menu-option-active", "")
   }
 
   // activate clicked tab and display its contents
-  document.getElementById(menuOption).style.display = "block";
+  document.getElementById(menuOption).style.display = "block"
   evt.currentTarget.className += " menu-option-active"
 }
